75df7ca868deeacba32967454714dae6cb456b6c
Ahn sena <serendipity.sena@gmail.com>
2018-12-02 19:38:05 +0900

    HomePage에서 상위 n개 현재상영작 출력하기, 상영 예정작 버튼 클릭 시 movie 상태 변경하기

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
+import MovieList from '../containers/MovieList';
 
 export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: 'current',
+    };
+  }
+
+  handleMovie(movie) {
+    this.setState({
+      movie: movie,
+    });
+  }
   render() {
-    return <Layout />;
+    const { movie } = this.state;
+    return (
+      <Layout>
+        <button onClick={() => this.handleMovie('current')}>현재 상영작</button>
+        <button onClick={() => this.handleMovie('upcome')}>상영 예정작</button>
+        <MovieList page="home" movie={movie} />
+      </Layout>
+    );
   }
 }
