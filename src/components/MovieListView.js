b0b07dfc9d42897f3402337dfe780ba01bb7d313
Ahn sena <serendipity.sena@gmail.com>
2018-12-03 18:59:08 +0900

    change variable name to match real server data attribute

--- src/components/MovieListView.js
+++ src/components/MovieListView.js
@@ -8,13 +8,13 @@ export default class MovieListView extends Component {
     return (
       <ul className={s.movieList}>
         {list.map(l => (
-          <li key={l.id} className={s.movieItem}>
+          <li key={l.pk} className={s.movieItem}>
             {// movie: current이면 순위를 보여주고, upcomming이면 순위를 보여주지 않습니다.
             this.props.movie === 'current' ? (
-              <div className={s.rank}>{l.id}</div>
+              <div className={s.rank}>{l.pk}</div>
             ) : null}
             <figure>
-              <img src={l.postImg} alt={l.title} />
+              <img src={l.main_img_url} alt={l.title} />
               <figcaption>{l.title}</figcaption>
             </figure>
             <div className={s.button}>
