8c78af59aca960f6ad6813f2ee89a2831273aa1e
ohahthink <ohahthink@gmail.com>
2018-12-05 19:07:48 +0900

    수정사항반영

--- src/components/CarouselView.js
+++ src/components/CarouselView.js
@@ -5,12 +5,12 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default class CarouselView extends Component {
   render() {
-    // const { stillcuts } = this.props;
-    const stillcuts = [
-      'https://i.ytimg.com/vi/NiXtDZfo2js/maxresdefault.jpg',
-      'https://i.ytimg.com/vi/T3eJh8k__BQ/maxresdefault.jpg',
-      'https://brandinside.asia/wp-content/uploads/2018/11/5b80a85aabb59.jpg',
-    ];
+    const { stillcuts } = this.props;
+    // const stillcuts = [
+    //   'https://i.ytimg.com/vi/NiXtDZfo2js/maxresdefault.jpg',
+    //   'https://i.ytimg.com/vi/T3eJh8k__BQ/maxresdefault.jpg',
+    //   'https://brandinside.asia/wp-content/uploads/2018/11/5b80a85aabb59.jpg',
+    // ];
     return (
       <Carousel
         showArrows={true}
