ba44d8cfdcaa2f7852fb057faa93e8bd3b1bdaf7
westbro <westbro00@gmail.com>
2018-12-07 17:53:51 +0900

    메인 캐러셀 추가 스타일링

--- src/components/MainCarouselView.js
+++ src/components/MainCarouselView.js
@@ -9,10 +9,17 @@ export default class MainCarouselView extends Component {
     const captionStyle = {
       color: 'white',
       background: 'transparent',
-      fontSize: '2rem',
+      fontSize: '1.6rem',
       //   border: '1px solid red',
       textAlign: 'left',
-      textShadow: '1px 1px gray',
+      textShadow: '1px 1px black',
+      display: 'block',
+    };
+    const subCaptionStyle = {
+      display: 'block',
+      fontSize: '1.1rem',
+      color: '#ccc',
+      padding: '10px 0',
     };
     return (
       <Carousel
@@ -27,12 +34,18 @@ export default class MainCarouselView extends Component {
           <img src="https://t1.daumcdn.net/cfile/tistory/254300455885938421" />
           <p className="legend" style={captionStyle}>
             신비한 동물사전과 김혜비의 범죄
+            <span style={subCaptionStyle}>
+              Fantastic Beasts: The Crimes of Grindelwald
+            </span>
           </p>
         </div>
         <div>
           <img src="https://i.ytimg.com/vi/qSfnoV9ESng/maxresdefault.jpg" />
           <p className="legend" style={captionStyle}>
             Legend 2
+            <span style={subCaptionStyle}>
+              Fantastic Beasts: The Crimes of Grindelwald
+            </span>
           </p>
         </div>
       </Carousel>
