1563eda6ef99c33ebc02a5519a2cfdc3cc3e4900
westbro <westbro00@gmail.com>
2018-12-07 17:46:14 +0900

    메인 캐러셀 수정

--- src/components/MainCarouselView.js
+++ src/components/MainCarouselView.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import Carousel from 'nuka-carousel';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 import s from '../scss/MainCarouselView.module.scss';
 
 export default class MainCarouselView extends Component {
   render() {
+    const captionStyle = {
+      color: 'white',
+      background: 'transparent',
+      fontSize: '2rem',
+      //   border: '1px solid red',
+      textAlign: 'left',
+      textShadow: '1px 1px gray',
+    };
     return (
       <Carousel
-        width="70vw"
-        autoplay
-        autoplayInterval={1000}
-        transitionMode="fade"
+        autoPlay
+        showThumbs={false}
+        showIndicators={false}
+        infiniteLoop={true}
+        interval={2000}
+        className={s.carouselWrapper}
       >
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+        <div>
+          <img src="https://t1.daumcdn.net/cfile/tistory/254300455885938421" />
+          <p className="legend" style={captionStyle}>
+            신비한 동물사전과 김혜비의 범죄
+          </p>
+        </div>
+        <div>
+          <img src="https://i.ytimg.com/vi/qSfnoV9ESng/maxresdefault.jpg" />
+          <p className="legend" style={captionStyle}>
+            Legend 2
+          </p>
+        </div>
       </Carousel>
     );
   }
