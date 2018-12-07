7237e43ea48e488cadca9fe944f03fa40b2aa567
Ahn sena <serendipity.sena@gmail.com>
2018-12-07 11:12:19 +0900

    Base markup for Seat.js

--- src/containers/Seat.js
+++ src/containers/Seat.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import s from '../scss/Seat.module.scss';
 
 export default class Seat extends Component {
   render() {
-    return <div>좌석선택창구현예정</div>;
+    // const seatArr = [['A1',]]
+    return (
+      <div className={s.secondLine}>
+        <div className={s.seat}>좌석 선택창이 구현 될 예정입니다.</div>
+        <div className={s.side}>옵션 창입니다</div>
+      </div>
+    );
   }
 }
