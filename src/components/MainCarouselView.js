e3e50e29a7325dfbba52503448c3477c90317d5d
westbro <westbro00@gmail.com>
2018-12-10 21:26:56 +0900

    끄적거리기...

--- src/components/MainCarouselView.js
+++ src/components/MainCarouselView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import firstCarousel from './CGV_carousel_01.jpg';
 import secondCarousel from './CGV_carousel_02.jpg';
@@ -17,11 +18,12 @@ export default class MainCarouselView extends Component {
       textAlign: 'left',
       textShadow: '1px 1px black',
       display: 'block',
+      opacity: '1',
     };
     const subCaptionStyle = {
       display: 'block',
       fontSize: '1.1rem',
-      color: '#ccc',
+      color: '#9c9c9c',
       padding: '10px 0',
     };
     return (
@@ -30,32 +32,39 @@ export default class MainCarouselView extends Component {
         showThumbs={false}
         showIndicators={false}
         infiniteLoop={true}
-        interval={2000}
+        interval={2500}
+        stopOnHover={false}
         className={s.carouselWrapper}
       >
-        <div>
-          <img src={firstCarousel} alt="신비한 동물사전 영화포스터 이미지" />
-          <p className="legend" style={captionStyle}>
-            신비한 동물들과 그린델왈드의 범죄
-            <span style={subCaptionStyle}>
-              Fantastic Beasts: The Crimes of Grindelwald
-            </span>
-          </p>
-        </div>
-        <div>
-          <img src={secondCarousel} alt="보헤미안 랩소디 영화포스터 이미지" />
-          <p className="legend" style={captionStyle}>
-            보헤미안 랩소디
-            <span style={subCaptionStyle}>Bohemian Rhapsody</span>
-          </p>
-        </div>
-        <div>
-          <img src={thirdCarousel} alt="스윙키즈 영화포스터 이미지" />
-          <p className="legend" style={captionStyle}>
-            스윙키즈
-            <span style={subCaptionStyle}>Swing Kids</span>
-          </p>
-        </div>
+        <Link to="/movies/detail/10" className={s.logo}>
+          <div>
+            <img src={firstCarousel} alt="신비한 동물사전 영화포스터 이미지" />
+            <p className="legend" style={captionStyle}>
+              신비한 동물들과 그린델왈드의 범죄
+              <span style={subCaptionStyle}>
+                Fantastic Beasts: The Crimes of Grindelwald
+              </span>
+            </p>
+          </div>
+        </Link>
+        <Link to="/movies/detail/5" className={s.logo}>
+          <div>
+            <img src={thirdCarousel} alt="스윙키즈 영화포스터 이미지" />
+            <p className="legend" style={captionStyle}>
+              스윙키즈
+              <span style={subCaptionStyle}>Swing Kids</span>
+            </p>
+          </div>
+        </Link>
+        <Link to="/movies/detail/1" className={s.logo}>
+          <div>
+            <img src={secondCarousel} alt="보헤미안 랩소디 영화포스터 이미지" />
+            <p className="legend" style={captionStyle}>
+              보헤미안 랩소디
+              <span style={subCaptionStyle}>Bohemian Rhapsody</span>
+            </p>
+          </div>
+        </Link>
       </Carousel>
     );
   }
