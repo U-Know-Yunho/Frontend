5df9d75bac2a9f3384424ffd1a762ea36b08ae38
ohahthink <ohahthink@gmail.com>
2018-12-10 20:21:36 +0900

    캐러셀 이미지 삽입

--- src/components/MainCarouselView.js
+++ src/components/MainCarouselView.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import firstCarousel from './CGV_carousel_01.jpg';
+import secondCarousel from './CGV_carousel_02.jpg';
+import thirdCarousel from './CGV_carousel_03.jpg';
 
 import s from '../scss/MainCarouselView.module.scss';
 
@@ -31,19 +34,26 @@ export default class MainCarouselView extends Component {
         className={s.carouselWrapper}
       >
         <div>
-          <img src="https://t1.daumcdn.net/cfile/tistory/254300455885938421" />
+          <img src={firstCarousel} alt="신비한 동물사전 영화포스터 이미지" />
           <p className="legend" style={captionStyle}>
-            신비한 동물들과 김혜비의 범죄
+            신비한 동물들과 그린델왈드의 범죄
             <span style={subCaptionStyle}>
               Fantastic Beasts: The Crimes of Grindelwald
             </span>
           </p>
         </div>
         <div>
-          <img src="https://i.ytimg.com/vi/d8Xaqr4sIX8/maxresdefault.jpg" />
+          <img src={secondCarousel} alt="보헤미안 랩소디 영화포스터 이미지" />
           <p className="legend" style={captionStyle}>
             보헤미안 랩소디
             <span style={subCaptionStyle}>Bohemian Rhapsody</span>
+          </p>
+        </div>
+        <div>
+          <img src={thirdCarousel} alt="스윙키즈 영화포스터 이미지" />
+          <p className="legend" style={captionStyle}>
+            스윙키즈
+            <span style={subCaptionStyle}>Swing Kids</span>
           </p>
         </div>
       </Carousel>
