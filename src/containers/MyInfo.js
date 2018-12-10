06e3c63c756443dabaf8339311e69493e32dafdd
Ahn sena <serendipity.sena@gmail.com>
2018-12-10 19:21:58 +0900

    modified interceptor about include token at headers when request

--- src/containers/MyInfo.js
+++ src/containers/MyInfo.js
@@ -18,30 +18,15 @@ export default class MyInfo extends Component {
   }
   // 현재 로그인 중인 유저 정보 가져와서 상태에 넣어주기
   async componentDidMount() {
-    // FIX: 헤더 조건 basic Auth로 설정되어있음. bearer auth로 변경 요청하기
-    // const token = localStorage.getItem('token');
-    // const { data } = await api.get('api/members/profile/', {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // });
     const {
       username,
-      last_name: lastname,
-      first_name: firstname,
+      lastName: lastname,
+      firstName: firstname,
       email,
-      phone_number: phonenumber,
-    } = {
-      pk: 1,
-      username: 'team4',
-      password:
-        'pbkdf2_sha256$120000$p8QbCth4UNV2$cUxOhFiCSNvUzcdDXHxP6YX4zs9QErSevN6tXwzGCfs=',
-      last_name: '정',
-      first_name: '윤호',
-      email: 'test@test',
-      phone_number: '010-0000-0000',
-    };
+      phoneNumber: phonenumber,
+    } = await api.get('/api/members/profile/');
 
+    console.log(lastname);
     this.setState({
       username,
       lastname,
@@ -60,23 +45,14 @@ export default class MyInfo extends Component {
 
   async handleSubmit(e) {
     e.preventDefault();
-    const token = localStorage.getItem('token');
     const { password, email, phonenumber } = this.state;
     try {
       // password를 입력하지 않으면 submit요청이 일어나지 않음
-      await api.patch(
-        'api/members/profile/',
-        {
-          password,
-          email,
-          phonenumber,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      await api.patch('api/members/profile/', {
+        password,
+        email,
+        phonenumber,
+      });
       // 정보 수정이 성공적으로 되었을 때
       alert('회원정보가 성공적으로 수정되었습니다');
     } catch {
