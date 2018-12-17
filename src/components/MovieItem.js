62bf7f222b5516f618ab5982f98c94303353566c
Ahn sena <serendipity.sena@gmail.com>
2018-12-17 21:43:50 +0900

    Modified MovieItem: added opening-date & reservation score included css

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import withLoading from '../hoc/withLoading';
 import s from '../scss/MovieListView.module.scss';
 import c from 'classnames';
 
@@ -13,6 +12,10 @@ export default class MovieItem extends Component {
     };
   }
 
+  // componentDidMount() {
+  //   const date = this.props.date.splice(6);
+  //   console.log(date);
+  // }
   handleLoading() {
     this.setState({
       loading: false,
@@ -27,6 +30,9 @@ export default class MovieItem extends Component {
       i,
       thumbImgUrl,
       title,
+      ableReserve,
+      score,
+      date,
     } = this.props;
     return (
       <li
@@ -36,7 +42,7 @@ export default class MovieItem extends Component {
         })}
         onClick={() => onClass(pk)}
       >
-        {// movie: current이면 순위를 보여주고, upcomming이면 순위를 보여주지 않습니다.
+        {// 현재상영작이면 순위를 보여주고, 개봉예정작이면 순위를 보여주지 않습니다.
         movie === 'movies' && <div className={s.rank}>{i + 1}</div>}
         <figure>
           <img
@@ -66,14 +72,27 @@ export default class MovieItem extends Component {
             </div>
           )}
         </figure>
-        {/* <p className={s.date}>{l.openingDate}</p>
-    <p className={s.score}>{l.reservationScore}</p> */}
+        <div className={c({ [s.info]: movie === 'movies/pre' }, s.mobileInfo)}>
+          {movie === 'movies/pre' && (
+            <p className={s.date}>
+              <span>{date}</span> 개봉
+            </p>
+          )}
+          <p className={s.score}>{score}</p>
+        </div>
         <div className={s.button}>
-          <Link to={`/movies/detail/${pk}`}>상세정보</Link>
+          <Link
+            to={`/movies/detail/${pk}`}
+            className={c({ [s.single]: !ableReserve })}
+          >
+            상세정보
+          </Link>
           {/* 특정 영화의 예매버튼을 클릭하여 예매하기 페이지로 접속하면
     선택한 영화의 id를 예매페이지컴포넌트에서 match.params.movieId 프롭으로 접근할 수 있도록 
     해당 주소로 보냅시다. */}
-          <Link to={`/reservation/?moviePk=${pk}`}>예매하기</Link>
+          {ableReserve && (
+            <Link to={`/reservation/?moviePk=${pk}`}>예매하기</Link>
+          )}
         </div>
       </li>
     );
--- src/components/MovieListView.js
+++ src/components/MovieListView.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import withLoading from '../hoc/withLoading';
 import s from '../scss/MovieListView.module.scss';
 import c from 'classnames';
@@ -36,6 +35,9 @@ class MovieListView extends Component {
               i={i}
               thumbImgUrl={l.thumbImgUrl}
               title={l.title}
+              ableReserve={l.nowShow}
+              score={l.reservationScore}
+              date={l.openingDate}
             />
           ))}
           {/* flex: space-between 마지막 줄에 아이템이 몇개 있든 윗 줄과 맞추려고 추가한 empty요소 */}
