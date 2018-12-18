c165ce9346f30d130a6316b1540f30b6b29aeee9
Ahn sena <serendipity.sena@gmail.com>
2018-12-18 21:42:25 +0900

    비로그인 중 마이페이지 주소 접근 시 로그인페이지로 리다이렉트 처리

--- src/pages/MyCGVPage.js
+++ src/pages/MyCGVPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import MyCGVLayout from '../components/MyCGVLayout';
-import MyCGVInfoPage from './MyCGVInfoPage';
 
 export default class MyCGVPage extends Component {
   render() {
