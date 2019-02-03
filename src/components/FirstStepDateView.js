47808a65b35533955409bf6acb14a509ab307e2d
westbro <westbro00@gmail.com>
2019-02-03 23:56:17 +0900

    예약 날짜시스템 버그 잡기

--- src/components/FirstStepDateView.js
+++ src/components/FirstStepDateView.js
@@ -3,99 +3,110 @@ import s from '../scss/FirstStepDateView.module.scss';
 import classNames from 'classnames';
 
 export default class FirstStepDateView extends Component {
-  handleDateList(list) {
-    return list.map(li => li.map(l => l[0].date.split('-')).map(t => t[2]));
-  }
-  handleDayList(list) {
-    const weekDay = ['일', '월', '화', '수', '목', '금', '토'];
-    const today = new Date();
-    let firstDay = today.getDay();
-    const dayArr = [];
-    for (const l of list) {
-      let arr = [];
-      for (let i = 0; i < l.length; i++) {
-        arr.push(weekDay[firstDay++ % 7]);
-      }
-      dayArr.push(arr);
+    handleDateList(list) {
+        return list.map(li => li.map(l => l[0].date.split('-')).map(t => t[2]));
     }
-    return dayArr;
-  }
-  getYear(date, n) {
-    const tmp = date.date.split('-')[0];
-    if (n === 2) return [parseInt(tmp), parseInt(tmp) + 1];
-    else return [tmp];
-  }
-  getMonth(date, n) {
-    const tmp = date.date.split('-')[1];
-    if (n === 2) return [parseInt(tmp), (parseInt(tmp) + 1) % 12];
-    else return tmp;
-  }
-  cutForNextMonth(list) {
-    const monthGroup = [];
-    list.forEach((item, idx) => {
-      if (item[0].date.split('-')[2] === '01') {
-        monthGroup.push(list.slice(0, idx));
-        monthGroup.push(list.slice(idx, list.length));
-      }
-    });
-    return monthGroup;
-  }
-  render() {
-    const { dateList } = this.props;
-    const monthGroup = this.cutForNextMonth(dateList);
-    let year, month;
-    try {
-      year = this.getYear(dateList[0][0], monthGroup.length);
-      month = this.getMonth(dateList[0][0], monthGroup.length);
-    } catch {}
-    return (
-      <div className={s.dateBox}>
-        {monthGroup.map((group, index) => (
-          <ul key={group}>
-            <li className={s.year}>{year[index]}</li>
-            <li className={s.month}>{month[index]}</li>
-            {this.renderTemplate(group, index)}
-          </ul>
-        ))}
-      </div>
-    );
-  }
+    handleDayList(list) {
+        const weekDay = ['일', '월', '화', '수', '목', '금', '토'];
+        const today = new Date();
+        let firstDay = today.getDay();
+        const dayArr = [];
+        for (const l of list) {
+            let arr = [];
+            for (let i = 0; i < l.length; i++) {
+                arr.push(weekDay[firstDay++ % 7]);
+            }
+            dayArr.push(arr);
+        }
+        return dayArr;
+    }
+    getYear(date, n) {
+        const tmp = date.date.split('-')[0];
+        if (n === 2) return [parseInt(tmp), parseInt(tmp) + 1];
+        else return [tmp];
+    }
+    getMonth(date, n) {
+        const tmp = date.date.split('-')[1];
+        if (n === 2) return [parseInt(tmp), (parseInt(tmp) + 1) % 12];
+        else return [tmp];
+    }
+    cutForNextMonth(list) {
+        const monthGroup = [];
+        list.forEach((item, idx) => {
+            if (item[0].date.split('-')[2] === '01') {
+                monthGroup.push(list.slice(0, idx));
+                monthGroup.push(list.slice(idx, list.length));
+            }
+        });
+        if (monthGroup.length === 0) {
+            monthGroup.push(list);
+        }
+        return monthGroup;
+    }
+    render() {
+        const { dateList } = this.props;
+        const monthGroup = this.cutForNextMonth(dateList);
+        let year, month;
+        try {
+            year = this.getYear(dateList[0][0], monthGroup.length);
+            month = this.getMonth(dateList[0][0], monthGroup.length);
+        } catch { }
+        return (
+            <div className={s.dateBox}>
+                {monthGroup.map((group, index) => (
+                    <ul key={group}>
+                        {
+                            year ? (
+                                <li className={s.year}>{year[index]}</li>
+                            ) : null
+                        }
+                        {
+                            month ? (
+                                <li className={s.month}>{month[index]}</li>
+                            ) : null
+                        }
+                        {this.renderTemplate(group, index)}
+                    </ul>
+                ))}
+            </div>
+        );
+    }
 
-  renderTemplate = (group, index) => {
-    const { handleDateClick, date, dateList, handleInvalidClick } = this.props;
-    const monthGroup = this.cutForNextMonth(dateList);
-    const dateGroup = this.handleDateList(monthGroup);
-    const dayGroup = this.handleDayList(monthGroup);
+    renderTemplate = (group, index) => {
+        const { handleDateClick, date, dateList, handleInvalidClick } = this.props;
+        const monthGroup = this.cutForNextMonth(dateList);
+        const dateGroup = this.handleDateList(monthGroup);
+        const dayGroup = this.handleDayList(monthGroup);
 
-    return group.map((d, idx) =>
-      d[1].show ? (
-        <li
-          key={d[0].date}
-          onClick={() => handleDateClick(d[0].date)}
-          className={classNames(
-            {
-              [s.selected]: d[0].date === date,
-            },
-            [s.dateLi]
-          )}
-        >
-          <div className={s.dateAndDay}>
-            <span>{dayGroup[index][idx]}</span>
-            <span className={s.date}>{dateGroup[index][idx]}</span>
-          </div>
-        </li>
-      ) : (
-        <li
-          key={d[0].date}
-          onClick={() => handleInvalidClick()}
-          className={s.movieNoneLi}
-        >
-          <div className={s.dateAndDay}>
-            <span>{dayGroup[index][idx]}</span>
-            <span className={s.date}>{dateGroup[index][idx]}</span>
-          </div>
-        </li>
-      )
-    );
-  };
+        return group.map((d, idx) =>
+            d[1].show ? (
+                <li
+                    key={d[0].date}
+                    onClick={() => handleDateClick(d[0].date)}
+                    className={classNames(
+                        {
+                            [s.selected]: d[0].date === date,
+                        },
+                        [s.dateLi]
+                    )}
+                >
+                    <div className={s.dateAndDay}>
+                        <span>{dayGroup[index][idx]}</span>
+                        <span className={s.date}>{dateGroup[index][idx]}</span>
+                    </div>
+                </li>
+            ) : (
+                    <li
+                        key={d[0].date}
+                        onClick={() => handleInvalidClick()}
+                        className={s.movieNoneLi}
+                    >
+                        <div className={s.dateAndDay}>
+                            <span>{dayGroup[index][idx]}</span>
+                            <span className={s.date}>{dateGroup[index][idx]}</span>
+                        </div>
+                    </li>
+                )
+        );
+    };
 }
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -8,118 +8,117 @@ import menu from '../scss/menu.png';
 import close from '../scss/btn_close.png';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      active: false,
-    };
-  }
+        this.state = {
+            active: false,
+        };
+    }
 
-  handleMenuActive() {
-    this.setState({
-      active: !this.state.active,
-    });
-  }
+    handleMenuActive() {
+        this.setState({
+            active: !this.state.active,
+        });
+    }
 
-  render() {
-    const { isLogin, logout, history, firstName, lastName } = this.props;
-    const currentLocation = this.props.location.pathname;
-    console.log(currentLocation);
-    return (
-      <div className={classNames(s.menuBar, { [s.active]: this.state.active })}>
-        <img
-          src={menu}
-          alt="모바일메뉴버튼"
-          className={s.menuButton}
-          onClick={() => this.handleMenuActive()}
-        />
-        <Link to="/" className={s.logo}>
-          Home
+    render() {
+        const { isLogin, logout, history, firstName, lastName } = this.props;
+        const currentLocation = this.props.location.pathname;
+        return (
+            <div className={classNames(s.menuBar, { [s.active]: this.state.active })}>
+                <img
+                    src={menu}
+                    alt="모바일메뉴버튼"
+                    className={s.menuButton}
+                    onClick={() => this.handleMenuActive()}
+                />
+                <Link to="/" className={s.logo}>
+                    Home
         </Link>
-        <div className={classNames(s.menu)}>
-          {lastName !== '' && firstName !== '' && (
-            <Link to="/myCGV/info" className={s.username}>
-              {lastName}
-              {firstName}님
+                <div className={classNames(s.menu)}>
+                    {lastName !== '' && firstName !== '' && (
+                        <Link to="/myCGV/info" className={s.username}>
+                            {lastName}
+                            {firstName}님
             </Link>
-          )}
-          <img
-            src={close}
-            alt="모바일메뉴 닫힘 버튼"
-            className={s.menuClose}
-            onClick={() => this.handleMenuActive()}
-          />
-          <Link
-            to="/movies"
-            className={classNames([s.link], {
-              [s.HeaderFocus]:
-                currentLocation === '/movies' || currentLocation === '/movies/',
-            })}
-          >
-            Movie
+                    )}
+                    <img
+                        src={close}
+                        alt="모바일메뉴 닫힘 버튼"
+                        className={s.menuClose}
+                        onClick={() => this.handleMenuActive()}
+                    />
+                    <Link
+                        to="/movies"
+                        className={classNames([s.link], {
+                            [s.HeaderFocus]:
+                                currentLocation === '/movies' || currentLocation === '/movies/',
+                        })}
+                    >
+                        Movie
           </Link>
-          <Link
-            to="/reservation"
-            className={classNames([s.link], {
-              [s.HeaderFocus]: currentLocation === '/reservation',
-            })}
-          >
-            Reservation
+                    <Link
+                        to="/reservation"
+                        className={classNames([s.link], {
+                            [s.HeaderFocus]: currentLocation === '/reservation',
+                        })}
+                    >
+                        Reservation
           </Link>
-          {isLogin ? (
-            <>
-              <button
-                onClick={() => {
-                  logout();
-                  history.push('/');
-                }}
-                className={s.link}
-              >
-                Logout
+                    {isLogin ? (
+                        <>
+                            <button
+                                onClick={() => {
+                                    logout();
+                                    history.push('/');
+                                }}
+                                className={s.link}
+                            >
+                                Logout
               </button>
-              <Link
-                to="/myCGV"
-                className={classNames([s.link], {
-                  [s.HeaderFocus]: currentLocation === '/myCGV',
-                })}
-              >
-                myCGV
+                            <Link
+                                to="/myCGV"
+                                className={classNames([s.link], {
+                                    [s.HeaderFocus]: currentLocation === '/myCGV',
+                                })}
+                            >
+                                myCGV
               </Link>
-            </>
-          ) : (
-            <>
-              <Link
-                to="/login"
-                className={classNames([s.link], {
-                  [s.HeaderFocus]: currentLocation === '/login',
-                })}
-              >
-                Login
+                        </>
+                    ) : (
+                            <>
+                                <Link
+                                    to="/login"
+                                    className={classNames([s.link], {
+                                        [s.HeaderFocus]: currentLocation === '/login',
+                                    })}
+                                >
+                                    Login
               </Link>
 
-              <Link
-                to="/register"
-                className={classNames([s.link], {
-                  [s.HeaderFocus]: currentLocation === '/register',
-                })}
-              >
-                Join
+                                <Link
+                                    to="/register"
+                                    className={classNames([s.link], {
+                                        [s.HeaderFocus]: currentLocation === '/register',
+                                    })}
+                                >
+                                    Join
               </Link>
-            </>
-          )}
-          <Link
-            to="/about"
-            className={classNames([s.link], {
-              [s.HeaderFocus]: currentLocation === '/about',
-            })}
-          >
-            About
+                            </>
+                        )}
+                    <Link
+                        to="/about"
+                        className={classNames([s.link], {
+                            [s.HeaderFocus]: currentLocation === '/about',
+                        })}
+                    >
+                        About
           </Link>
-        </div>
-      </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withRouter(withUser(Header));
