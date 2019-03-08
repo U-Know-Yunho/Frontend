3544bace9fefbb33e7651bd5694a4c394e4871a6
westbro <westbro00@gmail.com>
2019-03-08 19:33:11 +0900

    video autoplay 수정

--- src/components/CarouselView.js
+++ src/components/CarouselView.js
@@ -3,33 +3,33 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default class CarouselView extends Component {
-  render() {
-    const { stillcuts } = this.props;
-    return (
-      <Carousel
-        autoPlay={true}
-        showIndicators={false}
-        infiniteLoop={true}
-        interval={2000}
-      >
-        {/* map 내부 요소에는 스타일링이 안먹혀서 인라인 스타일링 줌 */}
-        {stillcuts.map((s, i) => (
-          <div
-            key={s}
-            style={{
-              height: '400px',
-              width: 'auto',
-              background: '#302f2a',
-            }}
-          >
-            <img
-              src={s}
-              style={{ height: 'inherit', width: 'inherit' }}
-              alt={i}
-            />
-          </div>
-        ))}
-      </Carousel>
-    );
-  }
+    render() {
+        const { stillcuts } = this.props;
+        return (
+            <Carousel
+                autoPlay={true}
+                showIndicators={false}
+                infiniteLoop={true}
+                interval={2000}
+            >
+                {/* map 내부 요소에는 스타일링이 안먹혀서 인라인 스타일링 줌 */}
+                {stillcuts.map((s, i) => (
+                    <div
+                        key={s}
+                        style={{
+                            height: '400px',
+                            width: 'auto',
+                            background: '#302f2a',
+                        }}
+                    >
+                        <img
+                            src={s}
+                            style={{ height: 'inherit', width: 'inherit' }}
+                            alt={i}
+                        />
+                    </div>
+                ))}
+            </Carousel>
+        );
+    }
 }
--- src/components/VideoView.js
+++ src/components/VideoView.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 
 export default class VideoView extends Component {
-  render() {
-    const opts = {
-      width: '100%',
-      playerVars: {
-        autoplay: 1,
-      },
-    };
-    const { trailer } = this.props;
+    render() {
+        const opts = {
+            width: '100%',
+            //   playerVars: {
+            //     autoplay: 1,
+            //   },
+        };
+        const { trailer } = this.props;
 
-    return <YouTube videoId={trailer} opts={opts} onReady={this._onReady} />;
-  }
+        return <YouTube videoId={trailer} opts={opts} onReady={this._onReady} />;
+    }
 
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  }
+    _onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
 }
